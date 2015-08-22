2ff033499b11329971dd2d5ccd18e78204d731d1
carusd <carusd@gmail.com>
2015-08-22 14:43:02 +0800

    work lib init

---
+++ worklib/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  worklib
+//
+//  Created by carusd on 15/8/22.
+//  Copyright (c) 2015年 carusd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ worklib/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  worklib
+//
+//  Created by carusd on 15/8/22.
+//  Copyright (c) 2015年 carusd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
