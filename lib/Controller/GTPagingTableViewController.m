d6737ac141e34bc355afb71d967d9ff23ca10867
carusd <carusd@gmail.com>
2015-09-25 18:03:49 +0800

    debug

--- lib/Controller/GTPagingTableViewController.m
+++ lib/Controller/GTPagingTableViewController.m
@@ -10,7 +10,7 @@
 
 @interface GTPagingTableViewController ()
 
-@property (nonatomic, strong) NSMutableArray *dataArray;
+//@property (nonatomic, strong) NSMutableArray *dataArray;
 @property (nonatomic, strong) GTNextPageIndicatorView *nextPageIndicatorView;
 
 @property (nonatomic, strong) NSLayoutConstraint *top4Refresh;
